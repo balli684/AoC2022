5cae3649a3a76e1dbb831698eba7c4b71cc51a25
Niels Ballis <niels@ballis.nl>
2022-12-06 23:58:16 +0100

    day 3 part 2

--- day3/part2.ps1
+++ day3/part2.ps1
@@ -4,13 +4,16 @@ $chars = $chars.ToCharArray()
 $puzzleinput = Get-Content .\input.txt
 $return = 0
 
-foreach ($line in $puzzleinput){
-    $first=($line.Substring(0,$line.Length/2)).ToCharArray()
-    $second=($line.Substring($line.Length/2)).ToCharArray()
-    $count=0
-    while (($count -lt $first.Count) -and (-not ($second.Contains($first[$count])))){
-        $count++
+for ($count = 0; $count -lt $puzzleinput.Count;$count+=3){
+    $one=($puzzleinput[$count]).ToCharArray()
+    $two=($puzzleinput[$count+1]).ToCharArray()
+    $three=($puzzleinput[$count+2]).ToCharArray()
+    $cnt = 0
+    while((-not($two.Contains($one[$cnt]))) -or (-not($three.Contains($one[$cnt]))) ){
+        $cnt++
     }
-    $return += [array]::IndexOf($chars,$first[$count])
+    $return += [array]::IndexOf($chars,$one[$cnt])
 }
+
 $return
+
