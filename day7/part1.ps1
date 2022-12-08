eaf55f6937875ce5970f89c8653c2e2717973064
Niels Ballis <niels@ballis.nl>
2022-12-08 22:35:42 +0100

    day 7

--- day7/part1.ps1
+++ day7/part1.ps1
@@ -1,32 +1,38 @@
 
-$puzzleinput = Get-Content .\input.txt
-# $return = 0
+$inputfile = ".\input.txt"
+$puzzleinput = Get-Content $inputfile
+$return = 0
 
-$currentdirs =  [System.Collections.ArrayList]@()
-$dirs =  [System.Collections.ArrayList]@()
+[System.Collections.ArrayList]$dirs = @()
+[hashtable]$sizes = @{}
 
 foreach ($line in $puzzleinput) {
     if ($line -like '$ cd *') {
         if (($line.Split(' '))[-1] -like '..'){
-            $dirs.Add($currentdirs[-1]) | Out-Null
-            $currentdirs.RemoveAt($currentdirs.Count-1)
+            $dirs.RemoveAt($dirs.Count-1)
         }
         else {
-            $currentdirs.Add(($line.Split(' '))[-1]) | Out-Null
+            $current=($line.Split(' '))[-1]
+
+            while ($sizes.$current -ge 0) {
+                $current += "_"
+            }
+            $dirs.Add($current) | Out-Null
+            $sizes.Add($dirs[-1],0) | Out-Null
         }
     } 
     elseif ((-not($line -like '$*')) -and (-not($line -like 'dir*'))) {
-        $size = [int]($line.Split(' '))[0]
+        foreach ($dir in $dirs) {
+            $sizes.$dir += [int]($line.Split(' '))[0]
+        }
 
     }
 }
-# $return
-while ($currentdirs) {
-    $dirs.Add($currentdirs[-1]) | Out-Null
-    $currentdirs.RemoveAt($currentdirs.Count-1)
+
+foreach ($value in $sizes.Values) {
+    if ($value -le 100000) {
+        $return += $value
+    }
 }
 
-Write-Host "current dirs are"
-$currentdirs
-Write-Host "dirs are"
-$dirs
+$return
